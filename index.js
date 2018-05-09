fd3f026e84e80dc2c88dc98cc71042c9d25b236a
Gavin Soko <sokolofg@gmail.com>
2018-05-09 22:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens;
 }
-function destructivelyRemoveLastKitten(name) {
-    return kittens.pop(name);
+function destructivelyRemoveLastKitten() {
+    return kittens.pop();
 }
 function appendKitten(name) {
   return [...kittens, name]
@@ -20,6 +20,10 @@ function appendKitten(name) {
 function removeLastKitten(){
   return kittens.slice(0, kittens.length - 1);
 }
-function removeFirstKitten(){
+function destructivelyRemoveFirstKitten(){
   return kittens.shift();
+}
+
+function removeFirstKitten(){
+  kittens.slice(1);
 }
